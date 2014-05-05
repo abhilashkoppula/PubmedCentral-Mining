738cbc27402778d1877ca7eb02ba6452aac8bd5c
abhilashkoppula <abhilashkoppula@gmail.com>
2014-05-05 14:19:55 -0400

    Removed output directories

--- dumpDirect/authors/author_2014-5-05-13-47-643.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO author VALUES(10,'Kenneth
-Evan','Thompson'),(11,'Caleb J.','Bashor'),(12,'Wendell
-A.','Lim'),(13,'Amy E.','Keating');
--- dumpDirect/category/category_reference_2014-5-05-13-47-646.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO category_reference VALUES(22558529,1000);
