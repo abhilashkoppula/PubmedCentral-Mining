a8588902e695c260b979f50463791c9563ad2446
scjensen <scott.jensen@sjsu.edu>
2014-06-16 20:12:30 -0700

    added constants fore single and double quote - considering switching single to double insterad of eliminating

--- src/edu/iub/pubmed/utils/Constants.java
+++ src/edu/iub/pubmed/utils/Constants.java
@@ -66,5 +66,9 @@ public class Constants {
 	
 	// Page Rank Threshold
 	public static final double PAGE_RANK_THRESHOLD = 10.00;	
+	
+	// Formatting Strings
+	public static final String DOUBLE_QUOTE = "\"";
+	public static final String SINGLE_QUOTE = "'";
 
 }
