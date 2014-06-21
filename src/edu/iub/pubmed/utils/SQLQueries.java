35636ef29bf608c1eb37e3559388510e1b8c1443
scott <scjensen@localhost.localdomain>
2014-06-21 07:41:31 -0400

    modified query strings for changes in tables and added insert for citation table

--- src/edu/iub/pubmed/utils/SQLQueries.java
+++ src/edu/iub/pubmed/utils/SQLQueries.java
@@ -15,7 +15,7 @@ public interface SQLQueries {
 	public static final String QUERY_INSERT_CATEGORY_REFERENCE_TABLE = "INSERT INTO category_reference VALUES";
 	public static final String QUERY_INSERT_CONFERENCE_TABLE = "INSERT INTO conference VALUES";
 	public static final String QUERY_INSERT_VOLUME_TABLE = "INSERT INTO volume VALUES";
-	//public static final String QUERY_INSERT_CITATION_TABLE = null;
+	public static final String QUERY_INSERT_CITATION_TABLE = "INSERT INTO citation VALUES";
 	public static final String QUERY_INSERT_PUBMED_REFERENCE_TABLE = "INSERT INTO citation_reference VALUES";
 	public static final String QUERY_INSERT_KEYWORD_TABLE = "INSERT INTO keyword VALUES";
 	public static final String QUERY_INSERT_KEYWORD_REFERENCE_TABLE = "INSERT INTO keyword_reference VALUES";
@@ -23,13 +23,14 @@ public interface SQLQueries {
 	
 	//public static final String QUERY_VALUES_ARTICLE_TABLE = "(%s,%s,'%s','%s',%s,%s)";
 	//public static final String QUERY_VALUES_AUTHOR_TABLE = "(%s,'%s','%s')";
-	public static final String QUERY_VALUES_AUTHOR_REFERENCE_TABLE = "(%s,%s)";
-	public static final String QUERY_VALUES_KEYWORD_TABLE = "(%s,'%s')";
-	public static final String QUERY_VALUES_KEYWORD_REFERENCE_TABLE = "(%s,%s)";
-	public static final String QUERY_VALUES_VOLUME_TABLE = "(%s,%s,%s,%s,%s)";
+	public static final String QUERY_VALUES_AUTHOR_REFERENCE_TABLE = "(%s,%s,%s)";
+	public static final String QUERY_VALUES_KEYWORD_TABLE = "(%s,'%s',%s)";
+	public static final String QUERY_VALUES_KEYWORD_REFERENCE_TABLE = "('%s',%s,%s)";
+	public static final String QUERY_VALUES_VOLUME_TABLE = "(%s,'%s','%s','%s','%s','%s')";
 	public static final String QUERY_VALUES_CONFERENCE_TABLE = "(%s,%s)";
 	//public static final String QUERY_VALUES_CATEGORY_TABLE = "(%s,'%s','%s')";
-	public static final String QUERY_VALUES_CATEGORY_REFERENCE_TABLE = "(%s,%s)";
+	public static final String QUERY_VALUES_CATEGORY_REFERENCE_TABLE = "(%s,%s,%s)";
+	public static final String QUERY_VALUES_CITATION_TABLE = "(%s,%s,%s,%s,%s)";
 	public static final String QUERY_VALUES_PUBMED_REFERENCE_TABLE = "(%s,%s,'%s','%s')";
 	
 	
