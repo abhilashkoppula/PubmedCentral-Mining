efc77a64a9da9be3ae8384ac390e48c0382e14d2
scott <scjensen@localhost.localdomain>
2014-06-25 23:48:35 -0400

    changed how volume and conference insert strings are formatted, so commented out the query format strings.  Converence had too few fields and we needed to address nulls.

--- src/edu/iub/pubmed/utils/SQLQueries.java
+++ src/edu/iub/pubmed/utils/SQLQueries.java
@@ -26,8 +26,8 @@ public interface SQLQueries {
 	public static final String QUERY_VALUES_AUTHOR_REFERENCE_TABLE = "(%s,%s,%s)";
 	public static final String QUERY_VALUES_KEYWORD_TABLE = "(%s,'%s',%s)";
 	public static final String QUERY_VALUES_KEYWORD_REFERENCE_TABLE = "('%s',%s,%s)";
-	public static final String QUERY_VALUES_VOLUME_TABLE = "(%s,'%s','%s','%s','%s','%s')";
-	public static final String QUERY_VALUES_CONFERENCE_TABLE = "(%s,%s)";
+	//public static final String QUERY_VALUES_VOLUME_TABLE = "(%s,'%s','%s','%s','%s','%s')";
+	//public static final String QUERY_VALUES_CONFERENCE_TABLE = "(%s,%s)";
 	//public static final String QUERY_VALUES_CATEGORY_TABLE = "(%s,'%s','%s')";
 	public static final String QUERY_VALUES_CATEGORY_REFERENCE_TABLE = "(%s,%s,%s)";
 	public static final String QUERY_VALUES_CITATION_TABLE = "(%s,%s,%s,%s,%s)";
